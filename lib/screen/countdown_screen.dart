194bc27cccce9467e86d0a2da30eaaa9b17d8199
Faisal Karim <Faisalmohammed2002@gmail.com>
2024-11-07 19:20:13 +1100

    Updated to latest libraries and removed deprecated ones. Also added pause/play functionality.

---
+++ lib/screen/countdown_screen.dart
@@ -0,0 +1,111 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:flutter/material.dart';
+import 'package:stretch_timer/model/CountDownInfo.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
+
+class CountdownScreen extends StatefulWidget {
+  static const routeName = 'countdown';
+
+  const CountdownScreen({super.key});
+
+  @override
+  _CountdownScreenState createState() => _CountdownScreenState();
+}
+
+class _CountdownScreenState extends State<CountdownScreen> {
+  final _controller = CountDownController();
+  CountDownInfo? timerInfo;
+  bool isStretching = true;
+  int iterationCount = 0;
+  final player = AudioPlayer();
+
+  @override
+  void initState() {
+    WakelockPlus.enable();
+    super.initState();
+  }
+
+  @override
+  void deactivate() {
+    WakelockPlus.disable();
+    super.deactivate();
+  }
+
+  void _timerIterationCompleted() async {
+    if (!isStretching) {
+      iterationCount++;
+    }
+
+    if (iterationCount == timerInfo!.intervalCount) {
+      await player.play(AssetSource('applause.mp3'));
+      Navigator.of(context).pop();
+      return;
+    } else if (isStretching) {
+      await player.play(AssetSource('notification.mp3'));
+    } else {
+      await player.play(AssetSource('buzzer.wav'));
+    }
+
+    setState(() {
+      isStretching = !isStretching;
+      _controller.restart();
+    });
+  }
+
+  void pause() {
+    _controller.pause();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    timerInfo = ModalRoute.of(context)!.settings.arguments as CountDownInfo;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Count down'),
+      ),
+      body: Column(
+        children: [
+          Text(
+            "${iterationCount + 1} / ${timerInfo!.intervalCount}",
+            style: const TextStyle(fontSize: 25),
+          ),
+          Center(
+            child: CircularCountDownTimer(
+              key: ValueKey(isStretching),
+              controller: _controller,
+              width: MediaQuery.of(context).size.width / 2,
+              height: MediaQuery.of(context).size.height / 2,
+              duration: isStretching
+                  ? timerInfo!.stretchDuration
+                  : timerInfo!.breakDuration,
+              isReverse: true,
+              isReverseAnimation: true,
+              strokeCap: StrokeCap.round,
+              strokeWidth: 20.0,
+              backgroundColor:
+                  isStretching ? Colors.green[400] : Colors.red[400],
+              fillColor: isStretching
+                  ? Colors.greenAccent[100]!
+                  : Colors.redAccent[100]!,
+              fillGradient: null,
+              ringColor: Colors.grey[300]!,
+              textStyle: const TextStyle(
+                fontSize: 33.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              onComplete: _timerIterationCompleted,
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () => _controller.pause(), child: const Text("Pause")),
+          ElevatedButton(
+              onPressed: () => _controller.resume(),
+              child: const Text("Resume")),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:stretch_timer/model/CountDownInfo.dart';
+import 'package:stretch_timer/screen/countdown_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = '/home';
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _form = GlobalKey<FormState>();
+
+  CountDownInfo _timerInfo = const CountDownInfo(30, 5, 3);
+  final _initial = {'stretch': '30', 'break': '5', 'iteration': '3'};
+
+  void _startTimer() {
+    if (!_form.currentState!.validate()) return;
+    _form.currentState!.save();
+
+    Navigator.of(context)
+        .pushNamed(CountdownScreen.routeName, arguments: _timerInfo);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stretches Timer'),
+      ),
+      body: Form(
+        key: _form,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              child: TextFormField(
+                initialValue: _initial['stretch'],
+                decoration: const InputDecoration(
+                  labelText: 'Stretch Duration (sec):',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a number';
+                  }
+                  if (int.tryParse(value) == null) {
+                    return 'Invalid number';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _timerInfo = CountDownInfo(
+                  int.parse(value!),
+                  _timerInfo.intervalCount,
+                  _timerInfo.breakDuration,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              child: TextFormField(
+                initialValue: _initial['break'],
+                decoration: const InputDecoration(
+                  labelText: 'Break Duration (sec):',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a number';
+                  }
+                  if (int.tryParse(value) == null) {
+                    return 'Invalid number';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _timerInfo = CountDownInfo(
+                  _timerInfo.stretchDuration,
+                  _timerInfo.intervalCount,
+                  int.parse(value!),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              child: TextFormField(
+                initialValue: _initial['iteration'],
+                decoration: const InputDecoration(
+                  labelText: 'Iteration Count',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a number';
+                  }
+                  if (int.tryParse(value) == null) {
+                    return 'Invalid number';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _timerInfo = CountDownInfo(
+                  _timerInfo.stretchDuration,
+                  int.parse(value!),
+                  _timerInfo.breakDuration,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: _startTimer,
+                child: const Text('Start'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
